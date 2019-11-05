7872a4698f95bd90538846225547bd0b1f6336ba
Tiago Martinho <tfmss1@iscte-iul.pt>
2019-11-05 10:03:05 +0000

    Initial structure

---
+++ CodacyChallenge/CommitModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodacyChallenge
+{
+    class CommitModel
+    {
+        public string Author { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ CodacyChallenge/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CodacyChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variável de ambiente, excepção se o utilizador não tiver o git instalado no sistema e definido como va
+            ProcessStartInfo startInfo = new ProcessStartInfo(@"D:\Dev\cmder\vendor\git-for-windows\bin\git.exe");
+
+            startInfo.UseShellExecute = true;
+            startInfo.WorkingDirectory = @"D:\Dev\boot-oauth2-draft";
+            startInfo.RedirectStandardInput = true;
+            startInfo.RedirectStandardOutput = true;
+            //startInfo.Arguments = "clone http://tk1:tk1@localhost/testrep.git";
+            startInfo.Arguments = "log";
+
+            Process process = new Process();
+            process.StartInfo = startInfo;
+            process.Start();
+
+            IList<CommitModel> commitList = new List<CommitModel>();
+            string lineVal = process.StandardOutput.ReadLine();
+
+            while (lineVal != null)
+            {
+
+                
+                lineVal = process.StandardOutput.ReadLine();
+
+            }
+
+            int val = output.Count();
+            process.WaitForExit();
+
+        }
+    }
+}
