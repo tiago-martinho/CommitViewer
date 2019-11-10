7f8d642bc6cbdb47350c948573c4e1d52a773216
Tiago Martinho <tfmss1@iscte-iul.pt>
2019-11-10 22:36:34 +0000

    Made Git exception message more clear

--- Domain/Utils/Git.cs
+++ Domain/Utils/Git.cs
@@ -4,14 +4,16 @@ namespace Domain.Utils
 {
     public static class Git
     {
+        private const string GitEnvironmentVariableKey = "GIT";
+
         public static string GitPath { get; private set; }
 
         public static void Setup()
         {
-            GitPath = Environment.GetEnvironmentVariable("GIT");
+            GitPath = Environment.GetEnvironmentVariable(GitEnvironmentVariableKey);
             if (string.IsNullOrWhiteSpace(GitPath))
             {
-                throw new InvalidOperationException("The GIT environment variable is not set. Please set the environment variable pointing to your git.exe");
+                throw new InvalidOperationException("The " + GitEnvironmentVariableKey + " environment variable is not set. Please set the environment variable with key: " + GitEnvironmentVariableKey + " and the value pointing to your git.exe");
             }
 
         }
